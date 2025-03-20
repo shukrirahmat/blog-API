ddf0234b404f05c2b3795e2d8f2e103d221288d3
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-03-20 22:57:43 +0800

    allow seeing all post without verifying

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -4,7 +4,7 @@ const postController = require("../controller/postController");
 const commentsRouter = require("./commentsRouter");
 const {verifyToken} = require("../controller/authentication")
 
-router.get("/", verifyToken, postController.getAllPosts);
+router.get("/", postController.getAllPosts);
 router.get("/:postId", verifyToken, postController.getPost);
 router.post("/", verifyToken, postController.postPost);
 router.delete("/:postId", verifyToken, postController.deletePost);
